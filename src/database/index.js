6a3a7fe64dffd2fcb8bbb1e80e12a8ae3454c06a
Luis Henrique <luish.fa1515@gmail.com>
2021-03-12 13:21:10 -0300

    Limpando os arquivos

--- src/database/index.js
+++
@@ -1,9 +0,0 @@
-import { createConnection, getConnectionOptions } from 'typeorm'
-
-export default async () => {
-    const defaultOptions = await getConnectionOptions()
-    
-    return createConnection(
-        Object.assign(defaultOptions, defaultOptions.database)
-    )
-}

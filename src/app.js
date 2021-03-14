951cec5c3b6d8fa809abf5ba881c1233804d92c8
Luis Henrique <luish.fa1515@gmail.com>
2021-03-14 13:41:32 -0300

    Fixando bugs

--- src/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.app = void 0;
-var express_1 = require("express");
-var routes_1 = require("./routes");
-var app = express_1["default"]();
-exports.app = app;
-app.use(express_1["default"].json());
-app.use(routes_1.router);
